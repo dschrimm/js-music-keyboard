e979e0b90a4389d5d9a4c95bb0db0a77375b51a2
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-11-21 14:50:59 -0800

    sounds is made when keyboard clicked or letters entered

--- noise.js
+++ noise.js
@@ -1,3 +1,23 @@
 $(document).ready( function() {
-  // your code here
+  $('.instrument').on('click', 'button', function(event) {
+    audio = getAudio($(this).html());
+    audio.load();
+    audio.play();
+  });
+
+  var getAudio = function(note) {
+    var id = note + ('Audio');
+    var audio = document.getElementById(id);
+
+    return audio;
+  };
+
+
+  $("body").keydown(function(event) {
+    var className = "note " + event.key;
+    audio = getAudio(event.key);
+    audio.load();
+    audio.play();
+  });
+
 });
